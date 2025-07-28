33da013dd158fe193756cb5acaeb1933ad6a9656
aswab007 <bsai23020@itu.edu.pk>
2025-07-28 16:54:46 +0500

    JS Variables & Objects

---
+++ 31st(JS Variables)/script.js
@@ -0,0 +1,22 @@
+let a = 3
+let b =4
+
+{
+    let a=44
+    console.log(a)
+}
+
+console.log(a)
+
+console.log(b)
+
+let obj={
+    "name":"Aswab",
+    "age":"20",
+    "religion":"Islam"
+}
+
+console.log(obj)
+
+obj.uni="ITU";
+console.log(obj)
