300b58e3ebaaa3d471ba5f0af6306b6db80d5b59
aswab007 <bsai23020@itu.edu.pk>
2025-07-28 17:32:25 +0500

    Loops

---
+++ 32nd(Conditionals+Loops)/script.js
@@ -0,0 +1,52 @@
+/*for (let i = 1; i < 100; i++) {
+    if(i%2==0)
+    {
+       console.log(i)
+    }
+     if(i%2!=0)
+    {
+        console.log(i)
+    }
+    
+}*/
+/*
+let obj={
+    name:" Aswab",
+    Age:" 20",
+    Religion:" Islam",
+    Uni:" ITU"
+}
+
+for (const key in obj) {
+
+    console.log(key+":"+obj[key])
+    
+}
+
+if (obj["name"]==" Aswab"){
+    console.log("Lessssgooo")
+}
+
+console.log(obj)
+*/
+
+/*
+const nums = [100, 200, 300];
+
+for (let num of nums) {
+  console.log(nums);
+}
+*/
+
+/*let stri = "ASWAB";
+for (const s of stri) {
+  console.log(s);
+}*/
+
+let i = 0;
+while (i < 10) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+  i++;
+}
