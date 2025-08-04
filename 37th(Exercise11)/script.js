32236d2b2b41d2dc3baca37029bbc04567167c90
aswab007 <bsai23020@itu.edu.pk>
2025-08-04 16:39:47 +0500

    Factorial Calculator

---
+++ 37th(Exercise11)/script.js
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////FACTORIAL/////////////////////////////////////////////////////////////////
+function facto(number){
+    let result =1
+    for (let i = number; i>0; i--) {
+
+        
+        result=result * i
+        
+    }
+    return result
+}
+
+console.log(facto(5))
+
+
+function fact(number){
+    
+}
