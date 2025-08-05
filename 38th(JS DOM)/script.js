9490bbae44a7a72904fae2a5f06262510ce71dd0
aswab007 <bsai23020@itu.edu.pk>
2025-08-05 17:01:58 +0500

    Intro to JS DOM

--- 38th(JS DOM)/script.js
+++ 38th(JS DOM)/script.js
@@ -5,4 +5,5 @@ console.log(document.body.children[4].previousElementSibling.style.backgroundCol
 console.log(document.body.children[4].parentElement.style.backgroundColor="red")
 console.log(document.body.firstElementChild.style.backgroundColor="blue")
 console.log(document.body.firstElementChild.nextElementSibling.style.backgroundColor="white")
+console.log(document.body.firstElementChild.nextElementSibling.nextElementSibling.style.backgroundColor="pink")
 
