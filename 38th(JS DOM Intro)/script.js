261ea61eeb3f17af4858d7a39415c8239d652681
aswab007 <bsai23020@itu.edu.pk>
2025-08-06 18:30:00 +0500

    IDK

---
+++ 38th(JS DOM Intro)/script.js
@@ -0,0 +1,9 @@
+console.log(document.body.children)
+console.log(document.body.childNodes)
+console.log(document.body.children[4].style.backgroundColor="green")
+console.log(document.body.children[4].previousElementSibling.style.backgroundColor="yellow")
+console.log(document.body.children[4].parentElement.style.backgroundColor="red")
+console.log(document.body.firstElementChild.style.backgroundColor="blue")
+console.log(document.body.firstElementChild.nextElementSibling.style.backgroundColor="white")
+console.log(document.body.firstElementChild.nextElementSibling.nextElementSibling.style.backgroundColor="pink")
+
---
+++ 39th(JS DOM Detailed)/script.js
@@ -0,0 +1,23 @@
+let boxx=document.getElementsByClassName("box")
+boxx[2].style.backgroundColor="red"
+
+document.getElementById("haha").style.backgroundColor="green"
+
+document.querySelector(".box").style.backgroundColor="Yellow" //used for the first occurence of the specified class
+
+let bex=document.querySelectorAll(".box")
+
+for (let i = 0; i < bex.length; i++) {
+    if(i%2==0){
+    bex[i].style.backgroundColor="aqua"  
+    }  
+}
+
+for (let i = 0; i < bex.length; i++) {
+    if(i%2!=0){
+    bex[i].style.backgroundColor="blue"  
+    }  
+}
+
+
+
